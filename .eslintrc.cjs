a9b6b4fa8d40005dfa5a90928f05775ecb1beda1
NeverEverFelix <felixmftc@icloud.com>
2025-11-16 21:01:20 -0500

    further projet set up[

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+// .eslintrc.cjs
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      project: "./tsconfig.json",
+      sourceType: "module"
+    },
+    plugins: ["@typescript-eslint"],
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended"
+    ],
+    env: {
+      node: true,
+      es2020: true
+    },
+    ignorePatterns: ["dist", "node_modules"]
+  };
+  
