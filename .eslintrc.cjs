94df8f25b66e45965a31d24b5ce35cfc8b40e7af
NeverEverFelix <felixmftc@icloud.com>
2025-12-01 18:33:53 -0500

    omfg

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,21 @@
 // .eslintrc.cjs
 module.exports = {
-    root: true,
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-      project: "./tsconfig.json",
-      sourceType: "module"
-    },
-    plugins: ["@typescript-eslint"],
-    extends: [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/recommended"
-    ],
-    env: {
-      node: true,
-      es2020: true
-    },
-    ignorePatterns: ["dist", "node_modules"]
-  };
-  
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    // either point at your real TS config:
+    project: "./tsconfig.build.json",
+    sourceType: "module",
+    ecmaVersion: 2020
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  env: {
+    node: true,
+    es2020: true
+  },
+  ignorePatterns: ["dist", "node_modules"]
+};
